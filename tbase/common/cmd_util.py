80d3070a34b266eb6dcbd04f17655eaafc77a75d
wen <liuwen.yzu@gmail.com>
2020-01-31 15:03:48 +0800

    args

--- tbase/common/cmd_util.py
+++ tbase/common/cmd_util.py
@@ -67,50 +67,23 @@ def common_arg_parser():
     Create an argparse.ArgumentParser for run_mujoco.py.
     """
     parser = arg_parser()
-    parser.add_argument('--env', help='environment ID', type=str, default='Reacher-v2')
-    parser.add_argument('--env_type', help='type of environment, used when the environment type cannot be automatically determined', type=str)
+    # 环境
+    parser.add_argument('--env', help='environment scenario', type=str, default='multi_vol')
+    parser.add_argument("--codes", type=str, default="000001.SZ,000002.SZ", help="tushare code of the experiment stocks")
+    parser.add_argument("--indexs", type=str, default="", help="tushare code of the indexs")
+    parser.add_argument("--start", type=str, default='20190101', help="when start the game")
+    parser.add_argument("--end", type=str, default='20191231', help="when end the game")
+    parser.add_argument("--invesment", type=float, default=100000, help="the invesment for each stock")
+    parser.add_argument("--look_back_days", type=int, default=10, help="how many days shoud look back")
+    parser.add_argument('--num_env', help='Number of environment copies being run in parallel.', default=None, type=int)
+    # 训练参数
     parser.add_argument('--seed', help='RNG seed', type=int, default=None)
     parser.add_argument('--alg', help='Algorithm', type=str, default='ppo2')
-    parser.add_argument('--num_timesteps', type=float, default=1e6),
+    parser.add_argument('--max_episode', type=float, default=1000)
+    # 模型参数
     parser.add_argument('--network', help='network type (mlp, cnn, lstm, cnn_lstm, conv_only)', default=None)
-    parser.add_argument('--gamestate', help='game state to load (so far only used in retro games)', default=None)
-    parser.add_argument('--num_env', help='Number of environment copies being run in parallel. When not specified, set to number of cpus for Atari, and to 1 for Mujoco', default=None, type=int)
-    parser.add_argument('--reward_scale', help='Reward scale factor. Default: 1.0', default=1.0, type=float)
     parser.add_argument('--save_path', help='Path to save trained model to', default=None, type=str)
-    parser.add_argument('--save_video_interval', help='Save video every x steps (0 = disabled)', default=0, type=int)
-    parser.add_argument('--save_video_length', help='Length of recorded video. Default: 200', default=200, type=int)
     parser.add_argument('--log_path', help='Directory to save learning curve data.', default=None, type=str)
+    # 运行参数
     parser.add_argument('--play', default=False, action='store_true')
     return parser
-
-def robotics_arg_parser():
-    """
-    Create an argparse.ArgumentParser for run_mujoco.py.
-    """
-    parser = arg_parser()
-    parser.add_argument('--env', help='environment ID', type=str, default='FetchReach-v0')
-    parser.add_argument('--seed', help='RNG seed', type=int, default=None)
-    parser.add_argument('--num-timesteps', type=int, default=int(1e6))
-    return parser
-
-
-def parse_unknown_args(args):
-    """
-    Parse arguments not consumed by arg parser into a dictionary
-    """
-    retval = {}
-    preceded_by_key = False
-    for arg in args:
-        if arg.startswith('--'):
-            if '=' in arg:
-                key = arg.split('=')[0][2:]
-                value = arg.split('=')[1]
-                retval[key] = value
-            else:
-                key = arg[2:]
-                preceded_by_key = True
-        elif preceded_by_key:
-            retval[key] = arg
-            preceded_by_key = False
-
-    return retval
