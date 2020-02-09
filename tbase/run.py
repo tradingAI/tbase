ef8a287816c42ea3db9d6259bde9272b02bd69a2
iminders <liuwen.w@qq.com>
2020-02-09 15:15:31 +0800

    run

--- tbase/run.py
+++ tbase/run.py
@@ -4,16 +4,16 @@
 2. 根据输入选择：算法，Policy-Net, Value-Net, 优化算法. 然后生成Agent
 3. 训练保存模型performance, 参数
 """
-import hydra
+import sys
+
 from tbase.common.logger import logger
 
 
-@hydra.main(config_path="config.yaml")
 def main(args):
     logger.info("running")
     logger.info("TODO")
-    print(args.pretty())
+    print(args)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
