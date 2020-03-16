c6a223e22cd959265f77bf8bd31fe688a6ff1d01
iminders <liuwen.w@qq.com>
2020-03-16 14:58:31 +0800

    gpu

--- build-docker-image-gpu.sh
+++ build-docker-image-gpu.sh
@@ -1,4 +1,4 @@
 # 需要先在环境变量中设置 TUSHARE_TOKEN
-docker build -f gpu.Dockerfile --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN --build-arg BUILD_TIMD=$(date +%s) . -t registry.cn-hangzhou.aliyuncs.com/aiminders/tbase:latest-gpu
+docker build -f gpu.Dockerfile --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN --build-arg BUILD_TIMD=$(date +%s) . -t registry.cn-hangzhou.aliyuncs.com/aiminders/tbase:gpu-latest
 # 重新 build
-# docker build --no-cache --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN . -t registry.cn-hangzhou.aliyuncs.com/aiminders/tbase:latest-gpu
+# docker build --no-cache --build-arg TUSHARE_TOKEN=$TUSHARE_TOKEN . -t registry.cn-hangzhou.aliyuncs.com/aiminders/tbase:gpu-latest
